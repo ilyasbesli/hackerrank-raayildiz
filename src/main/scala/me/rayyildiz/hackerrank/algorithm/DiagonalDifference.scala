9d78f15bea704c7bb4c726bd46787663a6d009aa
rayyildiz <rayyildiz@gmail.com>
2016-08-22 23:17:36 +0300

    "Diagonal Difference" submited https://www.hackerrank.com/challenges/diagonal-difference

---
+++ src/main/scala/me/rayyildiz/hackerrank/algorithm/DiagonalDifference.scala
@@ -0,0 +1,27 @@
+package me.rayyildiz.hackerrank.algorithm
+
+/**
+  * Created by rayyildiz on 8/22/16.
+  */
+object DiagonalDifference {
+
+  def main(args: Array[String]): Unit = {
+    val t = scala.io.StdIn.readInt()
+
+    val numbersStr = for ( i <- 0 until t) yield {
+      scala.io.StdIn.readLine()
+    }
+
+    val numbers = numbersStr.map(_.split(" ").take(t).map(_.toInt))
+
+    val sum1 = for ( i <- 0 until t) yield {
+      numbers(i)(i)
+    }
+
+    val sum2 = for ( i <- 0 until t) yield {
+      numbers(t - i -1)(i)
+    }
+
+    println(Math.abs(sum1.sum - sum2.sum))
+  }
+}
