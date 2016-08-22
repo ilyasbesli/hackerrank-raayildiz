0e0e79b3390d1ac2efe2c3a29c73ad710f45af06
rayyildiz <rayyildiz@gmail.com>
2016-08-22 23:07:29 +0300

    "A Very Big Sum" submited https://www.hackerrank.com/challenges/a-very-big-sum

---
+++ src/main/scala/me/rayyildiz/hackerrank/algorithm/VeryBigSum.scala
@@ -0,0 +1,18 @@
+package me.rayyildiz.hackerrank.algorithm
+
+/**
+  * Created by rayyildiz on 8/22/16.
+  */
+object VeryBigSum {
+
+  def main(args: Array[String]): Unit = {
+    val t = scala.io.StdIn.readInt()
+    val line = scala.io.StdIn.readLine()
+
+    val parts = line.split(" ").take(t)
+    val digits = for (part <- parts) yield  BigDecimal.apply(part)
+
+    println(digits.sum)
+  }
+
+}
