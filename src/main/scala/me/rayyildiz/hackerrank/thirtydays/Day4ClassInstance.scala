c963b0865dc07a7585313bdfb1cb09bf0d57377c
rayyildiz <rayyildiz@gmail.com>
2016-08-22 10:16:29 +0300

    "Day 4: Class vs. Instance" submited https://www.hackerrank.com/challenges/30-class-vs-instance

---
+++ src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day4ClassInstance.scala
@@ -0,0 +1,55 @@
+package me.rayyildiz.hackerrank.thirtydays
+
+/**
+  * Created by rayyildiz on 8/22/16.
+  */
+object Day4ClassInstance {
+  class Person {
+
+    var age: Int = 0
+
+    def this(initialAge:Int) = {
+      // Add some more code to run some checks on initialAge
+      this()
+      age = initialAge
+      if ( age < 1){
+        println("Age is not valid, setting age to 0.")
+        age = 0
+      }
+    }
+
+    def amIOld(): Unit = {
+      // Do some computations in here and print out the correct statement to the console
+      if ( age < 13) {
+        println("You are young.")
+      }else if ( age >=13 && age < 18) {
+        println("You are a teenager.")
+      }else {
+        println("You are old.")
+      }
+    }
+
+    def yearPasses(): Unit = {
+      // Increment the age of the person in here
+      age = age + 1
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val T=scala.io.StdIn.readInt()
+    val persons = for(i<-1 to T) yield {
+      val age = scala.io.StdIn.readInt()
+      new Person(age)
+    }
+
+    for (p <- persons){
+      p.amIOld()
+      for(j<-1 to 3){
+        p.yearPasses()
+      }
+      p.amIOld()
+      println()
+    }
+  }
+}
