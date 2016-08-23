a6aa79ca9b413deb00e4ec10e8b911961e6bf8a0
rayyildiz <rayyildiz@gmail.com>
2016-08-23 09:23:08 +0300

    "Day 7: Arrays" submited https://www.hackerrank.com/challenges/30-arrays

---
+++ src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day7Arrays.scala
@@ -0,0 +1,13 @@
+package me.rayyildiz.hackerrank.thirtydays
+
+/**
+  * Created by rayyildiz on 8/23/16.
+  */
+object Day7Arrays {
+
+  def main(args: Array[String]): Unit = {
+    val n = scala.io.StdIn.readInt()
+    val line = scala.io.StdIn.readLine()
+    println(line.split(" ").take(n).map(_.toInt).reverse.mkString(" "))
+  }
+}
