e7525f8561b66af18f935aef2332d288160a0c46
rayyildiz <rayyildiz@gmail.com>
2016-08-26 13:52:08 +0300

    "Day 8: Dictionaries and Maps" submited https://www.hackerrank.com/challenges/30-dictionaries-and-maps

--- src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day10BinaryNumbers.scala
+++ src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day10BinaryNumbers.scala
@@ -6,7 +6,6 @@ package me.rayyildiz.hackerrank.thirtydays
 object Day10BinaryNumbers {
 
   def main(args: Array[String]): Unit = {
-    val n = scala.io.StdIn.readInt()
-    println(Integer.toBinaryString(n).split("0").map(_.length).max)
+    println(Integer.toBinaryString(scala.io.StdIn.readInt()).split("0").map(_.length).max)
   }
 }
