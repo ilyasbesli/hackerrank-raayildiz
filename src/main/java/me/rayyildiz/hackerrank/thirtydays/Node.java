7a6d680944baa06c4ec2e3fbe1cf2bf9abe099e9
rayyildiz <rayyildiz@gmail.com>
2016-08-31 17:40:19 +0300

    "Day 15: Linked List" submited https://www.hackerrank.com/challenges/30-linked-list

---
+++ src/main/java/me/rayyildiz/hackerrank/thirtydays/Node.java
@@ -0,0 +1,13 @@
+package me.rayyildiz.hackerrank.thirtydays;
+
+/**
+ * Created by rayyildiz on 8/31/16.
+ */
+public class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
