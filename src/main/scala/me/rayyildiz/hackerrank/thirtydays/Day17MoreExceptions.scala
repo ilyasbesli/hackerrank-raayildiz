5c8f98d77b48d2543208977447de85b47f1d8bec
rayyildiz <rayyildiz@gmail.com>
2016-09-02 09:34:30 +0300

    "Day 17: More Exceptions" submited https://www.hackerrank.com/challenges/30-more-exceptions

---
+++ src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day17MoreExceptions.scala
@@ -0,0 +1,35 @@
+package me.rayyildiz.hackerrank.thirtydays
+
+/**
+  * Created by rayyildiz on 9/2/16.
+  */
+object Day17MoreExceptions {
+  class Calculator {
+
+    def power(n:Int, p:Int):Int = {
+      if ( n < 0 || p < 0) throw new Exception("n and p should be non-negative")
+      Math.pow(n,p).toInt
+    }
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+    val myCalculator=new Calculator()
+    var T=scala.io.StdIn.readLine().toInt
+
+    while(T>0){
+      val Array(n,p) = scala.io.StdIn.readLine().split(" ").map(_.toInt);
+      try{
+        val ans=myCalculator.power(n,p)
+        println(ans);
+      }
+      catch{
+        case e: Exception => {
+          println(e.getMessage())
+        }
+      }
+      T-=1
+    }
+  }
+}
