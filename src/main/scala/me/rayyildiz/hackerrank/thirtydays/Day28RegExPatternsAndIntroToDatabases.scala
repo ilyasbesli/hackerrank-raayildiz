5005b028effc2013fcdb3b7ff1b06481f1a927e1
rayyildiz <rayyildiz@gmail.com>
2016-09-19 10:26:01 +0300

    "Day 28: RegEx, Patterns, and Intro to Databases" submitted https://www.hackerrank.com/challenges/30-regex-patterns

---
+++ src/main/scala/me/rayyildiz/hackerrank/thirtydays/Day28RegExPatternsAndIntroToDatabases.scala
@@ -0,0 +1,22 @@
+package me.rayyildiz.hackerrank.thirtydays
+
+/**
+  * Created by rayyildiz on 9/19/16.
+  */
+object Day28RegExPatternsAndIntroToDatabases {
+
+
+  def main(args: Array[String]): Unit = {
+    val n = scala.io.StdIn.readInt()
+
+
+    val tmpNames = for( i<- 0 until n) yield scala.io.StdIn.readLine()
+
+    val names = tmpNames.toList.map(c => c.split(" ")).map(c => (c(0),c(1)))
+
+    val sorted = names.filter(_._2.endsWith("@gmail.com")).sortBy(_._1)
+
+    sorted.foreach(x => println(x._1))
+
+  }
+}
