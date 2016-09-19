cfae0517d73a5324f127d5e2014933b64b0b464b
rayyildiz <rayyildiz@gmail.com>
2016-09-19 10:02:21 +0300

    "Day 23: BST Level-Order Traversal" submitted https://www.hackerrank.com/challenges/30-binary-trees

---
+++ src/main/java/me/rayyildiz/hackerrank/thirtydays/Day23BSTLevelOrderTraversal.java
@@ -0,0 +1,53 @@
+package me.rayyildiz.hackerrank.thirtydays;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * Created by rayyildiz on 9/19/16.
+ */
+public class Day23BSTLevelOrderTraversal {
+
+    static void levelOrder(TreeNode root){
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        while (!q.isEmpty()){
+            TreeNode cur = q.remove();
+            System.out.print(cur.data+" ");
+            if ( cur.left != null) q.add(cur.left);
+            if ( cur.right!= null) q.add(cur.right);
+        }
+    }
+
+
+    public static TreeNode insert(TreeNode root,int data){
+        if(root==null){
+            return new TreeNode(data);
+        }
+        else{
+            TreeNode cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        TreeNode root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
