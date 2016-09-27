47c5509e4b5e36ca7d37497b520153bdaf21f793
rayyildiz <rayyildiz@gmail.com>
2016-09-27 10:20:12 +0300

    code format

--- src/main/scala/me/rayyildiz/hackerrank/algorithm/arrays/TwoArrayDS.scala
+++ src/main/scala/me/rayyildiz/hackerrank/algorithm/arrays/TwoArrayDS.scala
@@ -1,9 +1,8 @@
 package me.rayyildiz.hackerrank.algorithm.arrays
 
-
 object TwoArrayDS {
 
-  def main(args:Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
 
     // TODO delete it , it just a test code
     val tmp = """1 1 1 0 0 0
@@ -13,7 +12,7 @@ object TwoArrayDS {
 0 0 0 0 0 0
 0 0 0 0 0 0""".stripMargin
 
-    val square = for(i<- 0 until 6) yield (scala.io.StdIn.readLine().split(" ").take(6)).map(_.toInt).toList
+    val square = for (i <- 0 until 6) yield (scala.io.StdIn.readLine().split(" ").take(6)).map(_.toInt).toList
 
     // val square = tmp.split("\n").take(6).map(x => x.split(" ").take(6).map(y => y.toInt))
 
@@ -24,19 +23,14 @@ object TwoArrayDS {
     })
      */
 
-    val listOfSum = for(
-      i <- 1 until 5;
-      j <- 1 until 5
-    ) yield{
-      square(i-1)(j-1) + square(i-1)(j) + square(i-1)(j+1) +
-      square(i)(j) +
-      square(i+1)(j-1) + square(i+1)(j) + square(i+1)(j+1)
+    val listOfSum = for (i <- 1 until 5;
+                         j <- 1 until 5) yield {
+      square(i - 1)(j - 1) + square(i - 1)(j) + square(i - 1)(j + 1) +
+        square(i)(j) +
+        square(i + 1)(j - 1) + square(i + 1)(j) + square(i + 1)(j + 1)
     }
 
-
     println(listOfSum.max)
-
-    
     // println(square)
   }
 
